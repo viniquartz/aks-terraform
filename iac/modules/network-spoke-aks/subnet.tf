5b27a7f21b90f888eceaa788e726b6b8e9c4d164
Vinicius Santiago <vinicius_quartz@hotmail.com>
2025-09-21 16:16:33 -0300

    fix: subnet range

--- iac/modules/network-spoke-aks/subnet.tf
+++ iac/modules/network-spoke-aks/subnet.tf
@@ -2,7 +2,7 @@ resource "azurerm_subnet" "aks_subnet" {
   name                 = "${var.name_project}-vnet-spoke-subnet-aks"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet-spoke-aks.name
-  address_prefixes     = ["10.2.0.0/24"]
+  address_prefixes     = ["10.1.1.0/24"]
 
   # delegation {
   #   name = "aks_delegation"
